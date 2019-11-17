4f7fc95a27f3c5e47cd433210c39e2ad5a237db3
Jngamero <57850554+Jngamero@users.noreply.github.com>
2019-11-16 21:34:04 -0500

    Update Cafeteria.java

--- src/Cafeteria.java
+++ src/Cafeteria.java
@@ -11,7 +11,7 @@ public class Cafeteria {
 private double precio,cant, cant1, cant2, cant3, cant4, cant5, cant6 ,cant7, cant8, cant9, cant10, cant11, cant12, precio_total,jbtot,jbdes;
 private int lol;
 private String hola;
-
+private int hs;
 //Métodos de la clase
 public void FijarValor(double cantv, double preciov)
 {
